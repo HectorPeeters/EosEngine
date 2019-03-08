81be0dccacebd77433dc47bf54ddee27d0c23922
HectorPeeters <gMOZQRRRvaTzr24zJPK7>
2019-03-08 21:09:13 +0100

    Added basic Tiled map

---
+++ assets/tilemap/main.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Main" tilewidth="16" tileheight="16" tilecount="486" columns="27">
+ <image source="../textures/tilemap/tilemap_packed.png" width="432" height="288"/>
+ <terraintypes>
+  <terrain name="Grass" tile="56"/>
+  <terrain name="Sidewalk" tile="56"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0"/>
+ <tile id="1" terrain=",,0,0"/>
+ <tile id="2" terrain=",,0,"/>
+ <tile id="5" terrain="0,0,0,"/>
+ <tile id="6" terrain="0,0,,0"/>
+ <tile id="8" terrain=",,,1"/>
+ <tile id="9" terrain=",,1,1"/>
+ <tile id="10" terrain=",,1,"/>
+ <tile id="13" terrain="1,1,1,"/>
+ <tile id="14" terrain="1,1,,1"/>
+ <tile id="27" terrain=",0,,0"/>
+ <tile id="28" terrain="0,0,0,0"/>
+ <tile id="29" terrain="0,,0,"/>
+ <tile id="32" terrain="0,,0,0"/>
+ <tile id="33" terrain=",0,0,0"/>
+ <tile id="35" terrain=",1,,1"/>
+ <tile id="36" terrain="1,1,1,1"/>
+ <tile id="37" terrain="1,,1,"/>
+ <tile id="40" terrain="1,,1,1"/>
+ <tile id="41" terrain=",1,1,1"/>
+ <tile id="54" terrain=",0,,"/>
+ <tile id="55" terrain="0,0,,"/>
+ <tile id="56" terrain="0,,,"/>
+ <tile id="62" terrain=",1,,"/>
+ <tile id="63" terrain="1,1,,"/>
+ <tile id="64" terrain="1,,,"/>
+</tileset>
